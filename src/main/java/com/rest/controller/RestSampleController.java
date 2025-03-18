9a76c8cb038c8d1490eff014c65dcbd9b6aa783a
Shuchita Khurana <shuchita.khurana@kindredgroup.com>
2025-03-18 11:18:38 +0100

    Adding emp object

--- src/main/java/com/rest/controller/RestSampleController.java
+++ src/main/java/com/rest/controller/RestSampleController.java
@@ -1,13 +1,21 @@
 package com.rest.controller;
 
+import com.rest.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/v1/internal/employee")
 public class RestSampleController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    //todo check how data is exposed in url
+    @GetMapping("/employee")
+    public Employee hello(String caller) {
+        Employee employee = new Employee("Hey there");
+        employee.setEmail("test@test.com");
+        employee.setDepartment(caller);
+        return employee;
     }
+
 }
---
+++ src/main/java/com/rest/model/Employee.java
@@ -0,0 +1,74 @@
+package com.rest.model;
+
+import java.math.BigDecimal;
+
+public class Employee {
+    private String name;
+    private String email;
+    private String address;
+    private String phone;
+    private String department;
+    private BigDecimal salary;
+    private String dateOfBirth;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
+
